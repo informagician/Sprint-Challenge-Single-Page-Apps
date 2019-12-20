138ea79553686bb0337485a18a5f915393c1a76c
Milo Rastgoo <hello@informagician.com>
2019-12-20 11:57:35 -0500

    Routes created - Welcomepage linked to characterlist

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList.js";
 import { Route, Switch } from 'react-router-dom';
 import WelcomePage from './components/WelcomePage';
-import CharacterCard from "./components/CharacterCard.js";
 
 
 export default function App() {
   return (
     <main>
-      <Route exact path="/">
-        <WelcomePage />
-      </Route>
-      <Route path="/MovieList">
-        <Header />
-        <CharacterList />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <WelcomePage />
+        </Route>
+        <Route path="/characters">
+          <Header />
+          <CharacterList />
+        </Route>
+      </Switch>
     </main>
   );
 }
